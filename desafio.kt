648a691d2111582e23ad39c546a12d9733974167
Sandra <iglikowskicristiani@gmail.com>
2024-02-26 23:29:11 -0300

    desafio kotlin atualizado

--- desafio.kt
+++ desafio.kt
@@ -1,21 +1,39 @@
-// [Template no Kotlin Playground](https://pl.kotl.in/WcteahpyN)
+enum class Nivel { BASICO, INTERMEDIARIO, AVANCADO }
 
-enum class Nivel { BASICO, INTERMEDIARIO, DIFICIL }
+class Usuario(val nome: String, val email: String)
 
-class Usuario
-
-data class ConteudoEducacional(var nome: String, val duracao: Int = 60)
+data class ConteudoEducacional(val nome: String, val nivel: Nivel, val duracao: Int = 60)
 
 data class Formacao(val nome: String, var conteudos: List<ConteudoEducacional>) {
 
     val inscritos = mutableListOf<Usuario>()
     
     fun matricular(usuario: Usuario) {
-        TODO("Utilize o parâmetro $usuario para simular uma matrícula (usar a lista de $inscritos).")
+        inscritos.add(usuario)
+        println("${usuario.nome} se matriculou na ${this.nome}.")
+    }
+
+    fun listarConteudos() {
+        println("A formação ${this.nome} possui os seguintes conteúdos:")
+        for (conteudo in conteudos) {
+            println("- ${conteudo.nome} (${conteudo.nivel}, ${conteudo.duracao} minutos)")
+        }
     }
 }
 
 fun main() {
-    TODO("Analise as classes modeladas para este domínio de aplicação e pense em formas de evoluí-las.")
-    TODO("Simule alguns cenários de teste. Para isso, crie alguns objetos usando as classes em questão.")
+    val maria = Usuario("Maria", "maria@gmail.com")
+    val joao = Usuario("João", "joao@gmail.com")
+    val lucas = Usuario("Lucas", "lucas@gmail.com")
+
+    val kotlinBasico = ConteudoEducacional("KOTLIN", Nivel.BASICO)
+    val kotlinIntermediario = ConteudoEducacional("KOTLIN", Nivel.INTERMEDIARIO)
+    val kotlinAvancado = ConteudoEducacional("KOTLIN", Nivel.AVANCADO)
+
+    val formacaoKotlin = Formacao("Formação Kotlin", listOf(kotlinBasico, kotlinIntermediario, kotlinAvancado))
+
+    
+    formacaoKotlin.matricular(maria)
+    formacaoKotlin.matricular(joao)
+    formacaoKotlin.listarConteudos()
 }
